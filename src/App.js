c72ed0c2dfa15462d2823df13ce8f8229cd35b8d
Tomás <noronha.t@sapo.pt>
2023-03-26 23:03:01 +0100

    Body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import NavBar from "./components/NavBar";
+import "./App.css"
 
 function App() {
+
+  const projects = [
+    {
+      name: "Calculator",
+      link: "",
+    },
+    {
+      name: "Counter",
+      link: "",
+    },
+  ]
+
   return (
     <>
       <NavBar />
+          <h1 className="my-16 text-center text-5xl font-extrabold">Welcome.</h1>
+      <div className="flex px-10 gap-40">
+      <div style={{ width: "50%" }}>
+        <div>
+          <h1 className="text-3xl mb-4 font-bold">Hello</h1>
+        </div>
+        <div className="flex flex-col gap-6">
+          <p>
+            My name is Tomás Freire. I'm an 18 year-old self-taught
+            frontend-developer from Madeira, Portugal. I have a deep knowledge
+            of HTML, CSS, Javascript and React which I used to develop many
+            projects in order to build my developer portfolio
+          </p>
+          <p>
+            I love building building interactive and engaging websites and
+            applications. I also like the constant leraning and growth
+            opportunities that come with the job, as the field is always
+            evolving with different technologies. Being able to create something
+            that impacts users in a positive way is phenomenal
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2 className="text-3xl mb-4 font-bold">My projects</h2>
+        </div>
+        <div>
+          {projects.map((item) => (
+            <a className="list-item mx-5 text-lg font-semibold underline" href={item.link}>{item.name}</a>
+            ))}
+        </div>
+      </div>
+      </div>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,46 @@
-import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram";
+import React, { useState } from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MailIcon from "@mui/icons-material/Mail";
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 function NavBar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+    const body = document.body;
+    body.style.backgroundColor = darkMode ? "#fff" : "#000";
+    body.style.color = darkMode ? "#000" : "#fff";
+  }
   return (
     <>
-      <div className="bg-transparent h-20 w-full absolute top-0 flex justify-between items-center">
-        <div className="mx-4">
-            <img src="/59938655975.jpg" alt="" className="h-16" />
+      <div className="bg-transparent w-full top-0 flex justify-between px-8 mt-4">
+        <div className="">
+            {darkMode ?
+          <img src="\whiteletterslogo (1).png" alt="" className="h-14" />
+          :
+          <img src="\LettersLogoBlack.png" alt="" className="h-16" />
+        }
         </div>
-        <div className="mx-4">
-          <LinkedInIcon sx={{ color: "", fontSize: 40 }} />
-          <GitHubIcon sx={{ color: "", fontSize: 40 }} />
-          <MailIcon sx={{ color: "", fontSize: 40 }} />
+        <div className="flex gap-4">
+          <div className="relative top-1">
+            <button onClick={toggleDarkMode}>
+              {darkMode ? <NightlightIcon /> : <WbSunnyIcon />}
+            </button>
+          </div>
+          <div className="flex gap-4">
+            <a href="">
+              <LinkedInIcon sx={{ color: "", fontSize: 35 }} />
+            </a>
+            <a href="">
+              <GitHubIcon sx={{ color: "", fontSize: 35 }} />
+            </a>
+            <a href="">
+              <MailIcon sx={{ color: "", fontSize: 35 }} />
+            </a>
+          </div>
         </div>
       </div>
     </>
